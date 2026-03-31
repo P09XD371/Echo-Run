591355ce014cb5e63e98391ba12a5561b36ae4a7
P09XD371 <csgoforzarezor@gmail.com>
2026-03-31 18:25:56 +0500

    1

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
 {
     [Header("Player Component References")]
     [SerializeField] Rigidbody2D rb;
+    [SerializeField] Animator animator;
+    [SerializeField] SpriteRenderer sprite;
 
     [Header("Player Settings")]
     [SerializeField] float speed;
@@ -18,20 +20,33 @@ public class PlayerController : MonoBehaviour
 
     private float horizontal;
 
+    private void Update()
+    {
+        animator.SetFloat("Speed", Mathf.Abs(horizontal));
+        animator.SetBool("IsGrounded", IsGrounded());
+    }
+
     private void FixedUpdate()
     {
         rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
     }
 
     #region PLAYER_CONTROL
+
     public void Move(InputAction.CallbackContext context)
     {
         horizontal = context.ReadValue<Vector2>().x;
+
+        if (horizontal > 0)
+            sprite.flipX = false;
+
+        if (horizontal < 0)
+            sprite.flipX = true;
     }
 
     public void Jump(InputAction.CallbackContext context)
     {
-        if(context.performed && IsGrounded())
+        if (context.performed && IsGrounded())
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
         }
@@ -41,5 +56,6 @@ public class PlayerController : MonoBehaviour
     {
         return Physics2D.OverlapCircle(groundCheck.position, 0.3f, groundLayer);
     }
+
     #endregion
 }
